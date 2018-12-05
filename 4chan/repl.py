b9d160378022995821db2bf2d298a04e7037b292
Zane Lyle <zanelyle@gmail.com>
2018-12-04 22:40:59 -0600

    repl is functional

--- 4chan/repl.py
+++ 4chan/repl.py
@@ -2,6 +2,7 @@
 import os
 import gensim
 import readline
+import pickle
 from nltk.corpus import stopwords
 from nltk.stem import LancasterStemmer
 from nltk import download
@@ -10,70 +11,113 @@ import warnings
 warnings.filterwarnings('ignore', category=FutureWarning)
 
 download('stopwords')
-stemmer = LancasterStemmer()
 
 class Model:
-    def __init__(board):
+    def __init__(self, board):
+        self.board = 'news'
+        self.stemmer = LancasterStemmer()
+        self.load(board)
+
+    def build(self):
+        self.stem_map = dict()
+        print(f'building /{self.board}/ model')
+        stop_words = set(stopwords.words('english'))
+        with open(f'text/{self.board}/comments', 'r') as f:
+            posts = []
+            for post in f.readlines():
+                tokens = []
+                for word in post.split():
+                    if word not in stop_words:
+                        stemmed_word = self.stemmer.stem(word)
+                        if stemmed_word in self.stem_map.keys():
+                            self.stem_map[stemmed_word].add(word)
+                        else:
+                            self.stem_map[stemmed_word] = set([word])
+                        tokens.append(stemmed_word)
+                posts.append(tokens)
+        self.model = gensim.models.Word2Vec(posts, workers=8, size=250)
+        self.save()
+
+    def save(self):
+        self.model.save(f'models/{self.board}.model')
+        with open(f'models/{self.board}.map', 'wb') as f:
+            pickle.dump(self.stem_map, f)
 
     def load(self, board, force=False):
-        pass
-
-    def build_model(self, board):
-        print(f'loading /{board}/ model')
+        self.board = board
+        print(f'loading /{self.board}/ model')
         if force:
-            self.build_model(board)
+            self.build()
             return
         try:
-            self.model = gensim.models.Word2Vec.load(f'models/{board}.model')
+            self.model = gensim.models.Word2Vec.load(f'models/{self.board}.model')
+            with open(f'models/{self.board}.map', 'rb') as f:
+                self.stem_map = pickle.load(f)
         except FileNotFoundError:
-            self.build_model(board)
+            self.build()
 
-def build_model(board):
-    print(f'building /{board}/ model')
-    stop_words = set(stopwords.words('english'))
-    with open(f'text/{board}/comments', 'r') as f:
-        posts = [[stemmer.stem(word) for word in post.split() \
-                if word not in stop_words] \
-                for post in f.readlines()]
-    model = gensim.models.Word2Vec(posts, workers=8, size=250)
-    model.save(f'models/{board}.model')
-    return model
+    def find(self, user_input):
+        try:
+            split = user_input.split(' ')
+            word = split[0]
+            num = int(split[1])
+        except IndexError:
+            word = user_input
+            num = 10
+        try:
+            print(self.model.wv.most_similar(self.stemmer.stem(word), topn=num))
+        except KeyError:
+            print(f"{word} is not in the vocabulary")
 
-def load_model(board, force=False):
-    print(f'loading /{board}/ model')
-    if force:
-        return build_model(board)
-    try:
-        return gensim.models.Word2Vec.load(f'models/{board}.model')
-    except FileNotFoundError:
-        return build_model(board)
+    def compare(self, w1, w2):
+        w1 = self.stemmer.stem(split[1])
+        w2 = self.stemmer.stem(split[2])
+        print(model.wv.similarity(w1, w2))
 
-def expand(model, word):
-    pass
+    def expand(self, word):
+        try:
+            to_print = self.stem_map[word]
+        except KeyError:
+            to_print = f'Stem {word} not found in this model.'
+        print(to_print)
+
 
 def repl():
     user_input = ''
     board = 'news'
-    model = load_model(board)
+    model = Model(board)
     while True:
         user_input = input("> ")
         if ':q' in user_input:
             return
+
         elif ':o!' in user_input:
             board = user_input.split(' ')[1]
-            model = load_model(board, True)
+            model.load(board, True)
+
         elif ':o' in user_input:
             board = user_input.split(' ')[1]
-            model = load_model(board)
+            model.load(board)
+
         elif ':compare' in user_input:
-            w1 = stemmer.stem(user_input.split(' ')[1])
-            w2 = stemmer.stem(user_input.split(' ')[2])
-            print(model.wv.similarity(w1, w2))
+            split = user_input.split(' ')
+            if len(split) < 3:
+                print('Not enough args given.')
+                continue
+            w1 = split[1]
+            w2 = split[2]
+            model.compare(w1, w2)
+
+        elif ':e' in user_input:
+            split = user_input.split(' ')
+            if len(split) < 2:
+                print('No args given.')
+                continue
+            model.expand(split[1])
+
         else:
-            try:
-                print(model.wv.most_similar(stemmer.stem(user_input)))
-            except KeyError:
-                print(f">>> {user_input} is not in the vocabulary")
+            model.find(user_input)
+
 
 if __name__ == '__main__':
     if not os.path.exists('models'):
